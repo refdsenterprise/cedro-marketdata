7723bb0fc7d29ac958830811f8585cc580a2db74
Rafael Escaleira <rafael.escaleira@deliverit.com.br>
2022-10-02 15:23:50 -0400

    Remove path enum from domain

--- Sources/CedroWebSocketDomain/Helpers/Path.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-public enum Path: String {
-    case ws = "ws"
-    
-    func withReconnect(token: String) -> RawValue {
-        return self.rawValue + "?reconnect=\(token)"
-    }
-}
